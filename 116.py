cd90fc1ca1ec93204ec4a86c4af865c725faec30
tong <841241285@qq.com>
2020-10-15 17:03:21 +0800

    Problem 116 solved.

--- 116.py
+++ 116.py
@@ -49,6 +49,19 @@ class Solution:
             head = head.left
         return root
 
+# 方法三：递归
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        def dfs_travel(curr_node, next_node):
+            if curr_node is None:
+                return
+
+            curr_node.next = next_node
+            dfs_travel(curr_node.left, curr_node.right)
+            dfs_travel(curr_node.right, None if curr_node.next == None else curr_node.next.left)
+
+        dfs_travel(root, None)
+        return root
 
 """
 填充完美二叉树每个节点的next指针指向其下一个右侧节点（如果没有右侧节点则为空）
@@ -65,4 +78,12 @@ class Solution:
 2. 由于是完美二叉树，因此对遍历到的每个节点均有node.left.next->node.right，节点间均有pre_node.right.next->node.left；
 3. 遍历本层链表时，每个节点的child节点间右->左可以通过node.next是否存在来进行node.right.next->node.next.left；
 4. 本层遍历结束后，将head.left作为下一层的链表头节点继续进行遍历，即head=head.left，直到新head.left为None时则不存在下一层，问题解决。
+
+方法三：
+1. 深度优先搜索递归，由于是完美二叉树，node.left若存在则一定有node.left.next->node.right；
+2. 首先递归至树的最左端，进行左孩子的递归至叶子然后进行右孩子的判断，同时对每个经过的节点都使其与右侧节点进行链接（右侧没有则指向None）；
+3. curr_node.left递归一定存在右侧的curr_node.right进行链接；
+4. curr_node.right递归，由于是逐层链接递归至下方，因此递归的上一层若curr_node右侧节点存在，则已被curr_node.next所指向，若不存在则指向None；
+5. 使curr_node.right右孩子与curr_node.next.left（右侧节点的左孩子）或None（curr_node.next不存在，curr_node.right为该层最后一个）相链接；
+6. 深度优先搜索递归先左后右逐层向上，直至所有节点的右侧被指向，问题解决。
 """
